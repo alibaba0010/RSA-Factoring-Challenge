c64e94142b3b7b7dc7409d4bdd1ae6a5a39a4640
alibaba0010 <zakariyahali100@gmail.com>
2023-08-31 12:04:31 -0700

    RSA-Factoring-Challenge

---
+++ README.md
@@ -0,0 +1,2 @@
+RSA factoring Challenge
+The ultimate encryption solution developed by mathematicians and calculated by developers.
---
+++ factors.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+//	unsigned long long num = 1844674407370955169;
+	unsigned long long num = 1718944270642558716715;
+
+	printf("%llu\n", num);
+
+	return (0);
+}
